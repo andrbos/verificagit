3faf7f822eeaffd592d2edf133d3b0aa0699343a
Andrea Boschi <ANDREA.BOSCHI@local.ittcesena.it>
2023-03-18 09:17:54 +0100

    creazione della classe scuola

---
+++ Alunno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaGitù
+{
+     class Alunno
+    {
+        string nome, cognome, toString, restituzione ;
+        int età;
+        public Alunno (string nome, string cognome, string toString, string restituzione, int età)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.toString = toString;
+            this.restituzione = restituzione;   
+            this.età = età;
+               
+        }
+
+        public string getNome() { return nome; }
+        
+        public string getCognome() { return cognome; }
+
+        public string getToString() { return toString; }
+        public int getEtà() { return età; }
+        
+        public string getRestituzione(string restituzione) 
+        {
+            if (età > 16)
+            {
+                return "triennio";
+            }
+            else if(età < 16) { return "Biennio"; }
+            
+        }
+
+    }
+}
---
+++ scuola.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaGitù
+{
+     class scuola
+    {
+        string cognome, indirizzo;
+        int orariApertura, orariChiusura;
+        public scuola(string cognome, string indirizzo, int orariApertura, int orariChiusura) 
+        { 
+            this.cognome = cognome;
+            this.indirizzo = indirizzo; 
+            this.orariChiusura = orariChiusura;
+            this.orariApertura= orariApertura;
+        }
+    }
+}
